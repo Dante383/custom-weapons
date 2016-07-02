cb35548976d46d2fc45821c619e33636795199d0
Dante <dante383@protonmail.com>
2016-07-02 22:51:31 +0200

    disabled original weapons

---
+++ cw_c.lua
@@ -0,0 +1,8 @@
+﻿function init ()
+	-- disable original weapons
+	toggleControl("fire", false)
+	toggleControl("next_weapon", false)
+	toggleControl("previous_weapon", false)
+	toggleControl("aim_weapon", false)
+end
+addEventHandler("onClientResourceStart", root, init)
