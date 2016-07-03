bf2e247657fc76003d6e0d671ba83b1e6e55b21a
Dante <dante383@protonmail.com>
2016-07-03 02:24:35 +0200

    animations, yay

--- cw_s.lua
+++ cw_s.lua
@@ -27,15 +27,25 @@ addEventHandler("onPlayerConnect", root, onPlayerConnect)
 
 function onSlotChange (plr)
 	local currentWeapon = getElementData(plr, "current_weapon")
-	if currentWeapon.model then 
-		exports['bone_attach']:detachElementFromBone(currentWeapon.model)
-		destroyElement(currentWeapon.model)
+	local temp = getElementData(plr, "cw_temp") -- exist only if weapon has own model. maybe fix it?
+	if temp then
+		if getWeaponByID(temp.id).walking_style ~= 0 then 
+			exports['bone_attach']:detachElementFromBone(temp.model)
+			destroyElement(temp.model)
+			local ws = getElementData(plr, "walkingStyle") or 0 
+			setPedWalkingStyle(plr, ws)
+		end
 	end
 	local weapon = getWeaponByID(currentWeapon.id)
 	if weapon == false then return false end 
 	if weapon.modelid then 
 		currentWeapon.model = createObject(weapon.modelid, 0,0,0)
 		exports['bone_attach']:attachElementToBone(currentWeapon.model, plr, 12, 0, 0, 0, 0, -90, 0)
+		setElementData(plr, "walkingStyle", getPedWalkingStyle(plr))
+		setPedWalkingStyle(plr, weapon.walking_style)
+		setElementData(plr, "cw_temp", currentWeapon)
+	else 
+		setElementData(plr, "cw_temp", false)
 	end
 end
 
